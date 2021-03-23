944a23a555ec048c7330231644db7dcf8e48caa0
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2021-03-23 13:27:54 +0700

    login update

--- project/View/Login/LoginPage.aspx.cs
+++ project/View/Login/LoginPage.aspx.cs
@@ -13,10 +13,7 @@ namespace project.View
         private static Project_DatabaseEntities db = new Project_DatabaseEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.Cookies["remember"] != null)
-            {
-                Response.Redirect("https://google.com")
-            }
+            
         }
 
         protected void login_Click(object sender, EventArgs e)
@@ -24,19 +21,27 @@ namespace project.View
             string username = txtEmail.Text;
             string password = txtPassword.Text;
 
-            User user = (from x in db.Users where x.Username.Equals(username) && x.Password.Equals(password) select x).FirstOrDefault();
+            User currentUser = (from x in db.Users where x.Username.Equals(username) && x.Password.Equals(password) select x).FirstOrDefault();
 
-            if (user != null)
+            if (currentUser != null)
             {
                 if (cbRemember.Checked)
                 {
                     HttpCookie cookie = new HttpCookie("remember");
-                    cookie.Value = user.Id.ToString();
+                    cookie.Value = currentUser.Id.ToString();
                     cookie.Expires = DateTime.Now.AddHours(1);
                     Response.Cookies.Add(cookie);
                 }
 
-                Response.Redirect("https://google.com");
+                if(currentUser.RoleId == 1)
+                {
+                    Response.Redirect("BuyerHomePage.aspx");
+                }
+                else
+                {
+                    Response.Redirect("SellerrHomePage.aspx");
+                }
+                
             }
 
 
