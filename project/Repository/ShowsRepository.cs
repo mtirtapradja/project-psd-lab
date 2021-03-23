f579ae2d86a8f853f7e439adb35feadb15d055c4
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-03-23 12:14:15 +0700

    Repository Update

---
+++ project/Repository/ShowsRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using project.Model;
+using project.Factory;
+
+namespace project.Repository
+{
+    public class ShowsRepository
+    {
+        private static Project_DatabaseEntities db = new Project_DatabaseEntities();
+
+        public static bool insertShow(int sellerid, string name, int price, string description)
+        {
+            Show show = ShowFactory.Create(sellerid, name, price, description);
+
+            if (show != null)
+            {
+                db.Shows.Add(show);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static bool updateShow(int id, int sellerid, string name, int price, string description)
+        {
+            Show show = db.Shows.Find(id);
+
+            if (show != null)
+            {
+                show.SellerId = sellerid;
+                show.Name = name;
+                show.Price = price;
+                show.Description = description;
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+
+        public static bool deleteShow(int id)
+        {
+            Show show = db.Shows.Find(id);
+
+            if (show != null)
+            {
+                db.Shows.Remove(show);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ project/Repository/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using project.Factory;
+using project.Model;
+
+namespace project.Repository
+{
+    public class UserRepository
+    {
+        private static Project_DatabaseEntities db = new Project_DatabaseEntities();
+
+        public static bool insertUser(string username, string password, string name, int role)
+        {
+            User user = UserFactory.Create(username, password, name, role);
+
+            if (user != null)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
