9a410a744371bc09460c16ccb7448de9ca495a52
mtirtapradja <mtirtapradja@gmail.com>
2021-03-31 15:27:34 +0700

    commit last changes by Marcell

--- project/View/Login/LoginPage.aspx.cs
+++ project/View/Login/LoginPage.aspx.cs
@@ -11,9 +11,27 @@ namespace project.View
     public partial class LoginPage : System.Web.UI.Page
     {
         private static Project_DatabaseEntities db = new Project_DatabaseEntities();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            // Kalo udah ada cookie, berarti langsung redirect ke HomePage
+            if (Request.Cookies["remember"] != null)
+            {
+                int currentUserID = int.Parse(Request.Cookies["remember"].Value);
+
+                User currentUser = (from x in db.Users where x.Id.Equals(currentUserID) select x)
+                                .FirstOrDefault();
+
+                // Kalo user == Buyer redirect ke BuyerHomePage
+                if (currentUser.RoleId == 1)
+                {
+                    Response.Redirect("../HomePage/BuyerHomePage.aspx");
+                }
+                else
+                {
+                    Response.Redirect("../HomePage/SellerHomePage.aspx");
+                }
+            }
         }
 
         protected void login_Click(object sender, EventArgs e)
@@ -29,7 +47,7 @@ namespace project.View
                 if (cbRemember.Checked)
                 {
                     HttpCookie cookie = new HttpCookie("remember");
-                    cookie.Value = currentUser.Id.ToString();
+                    cookie.Value = currentUser.RoleId.ToString();
                     cookie.Expires = DateTime.Now.AddHours(1);
                     Response.Cookies.Add(cookie);
                 }
