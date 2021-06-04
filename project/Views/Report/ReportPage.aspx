35033665935e0004168f5cca9509d184bd5068d2
mtirtapradja <mtirtapradja@gmail.com>
2021-06-04 16:02:38 +0700

    add dataset, report file, and report page

---
+++ project/Views/Report/ReportPage.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Main.Master" AutoEventWireup="true" CodeBehind="ReportPage.aspx.cs" Inherits="project.Views.Report.ReportPage" %>
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <CR:CrystalReportViewer ID="crvTransactionDetail" runat="server" AutoDataBind="true" />
+</asp:Content>
---
+++ project/Views/Report/ReportPage.aspx.cs
@@ -0,0 +1,56 @@
+﻿using project.Controllers;
+using project.Datasets;
+using project.Models;
+using project.Report;
+using project.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace project.Views.Report
+{
+    public partial class ReportPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TransactionReport transactionReport = new TransactionReport();
+            crvTransactionDetail.ReportSource = transactionReport;
+            transactionReport.SetDataSource(GetDataItem());
+        }
+
+        protected DataSet GetData()
+        {
+            List<Show> shows = ShowController.GetAllShow();
+            DataSet dataset = new DataSet();
+
+            var dataset_shows = dataset.Show;
+            var dataset_transaction_header = dataset.TransactionHeader;
+            var dataset_transaction_detail = dataset.TransactionDetail;
+            var dataset_user = dataset.User;
+
+            foreach(var show in shows)
+            { 
+                var newShowRow = dataset_shows.NewRow();
+                newShowRow["Id"] = show.Id;
+                newShowRow["Name"] = show.Name;
+                newShowRow["TotalPrice"] = show.Price;
+                dataset_shows.Rows.Add(newShowRow);
+
+                List<TransactionHeader> headers = TransactionController.GetAllTransactionHeaderByShowId(show.Id); 
+
+                foreach(var header in headers)
+                {
+                    var newHeaderRow = dataset_transaction_header.NewRow();
+                    newHeaderRow["BuyerId"] = header.BuyerId;
+                    newHeaderRow["CreatedAt"] = header.CreatedAt;
+                    dataset_transaction_header.Rows.Add(newHeaderRow);
+                }
+            }
+
+            return dataset;
+        }
+    }
+}
